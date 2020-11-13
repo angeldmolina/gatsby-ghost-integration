c8cee8a8c17c38a1ebabf28c8f7a07ac60e0f6aa
angeldmolina <theangeldmolina@gmail.com>
2020-11-13 19:39:17 +0000

    favicon (#10)

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://frebabylon5.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://freebabylon5.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
